6facd8711662785e484d724f6851bb5ddb659edb
alexey257 <alexey_vasilyev-@mail.ru>
2015-09-16 09:53:25 +0300

    Задание со слагаемыми заработало, но код некрасивый

--- sums/sums/main.cpp
+++ sums/sums/main.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 
 
-int make_sum(int* mas, char* my_string, int num)
+int make_sum(int* mas, int max_idx, char* my_string, int num)
 {
-    //int length = 0;
     int int_length = 0;
+
     while(num) //fill new string
     {
-        my_string[int_length] = mas[int_length] + 0x30;
-        num -= mas[int_length];
+        my_string[int_length] = mas[max_idx] + 0x30;
+        num -= mas[max_idx];
+        max_idx--;
         int_length++;
     }
     my_string[int_length] = '\0';
@@ -23,19 +24,23 @@ void enum_combs(int mas[], int i, int num, char* my_string)
 {
     //int ln = 0;
     int sum = 0;
-    while(sum < num)
+    while(sum <= num)
     {
-        make_sum(mas, my_string, num);
+        sum = 0;
         if(i) enum_combs(mas, (i-1), num, my_string);
         mas[i]++;
-        for(int j = 0; j <= i; j++)
+        int j = 0;
+        while(sum < num)
         {
             sum += mas[j];
+            j++;
         }
+        //}
+        if((sum <= num)&&(mas[i] < num)) make_sum(mas, (j-1), my_string, num);
+        //sum = 0;
     }
     mas[i] = 1;
     //mas[i++]++;
-
 }
 
 void number_parse(const int num)
@@ -47,6 +52,7 @@ void number_parse(const int num)
         mas[i] = 1; //fill with ones
     }
     //int sum = 0;
+    make_sum(mas, num-1, my_string, num);
     enum_combs(mas, num-1, num, my_string);
 }
 
